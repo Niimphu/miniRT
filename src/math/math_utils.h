94803f0ff7f92fe2ce2971a6fac5fa03b94dafb8
Kekuhne <kekuhne@student.42Wolfsburg.de>
2023-12-06 19:26:34 +0100

    added basic vector math function to math_utils.c(not tested)

---
+++ src/math/math_utils.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   math_utils.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Kekuhne <kekuehne@student.42wolfsburg.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 17:29:29 by Kekuhne           #+#    #+#             */
+/*   Updated: 2023/12/06 18:19:58 by Kekuhne          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MATH_UTILS_H
+# define MATH_UTILS_H
+
+typedef struct s_vector 
+{
+	float x;
+	float y;
+	float z;
+} t_vector;
+
+
+#endif
