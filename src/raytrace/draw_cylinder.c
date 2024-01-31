3b0cfb5d9bfdc1e1eb5afd22f02cbff174860b1a
Kekuhne <kekuhne@student.42Wolfsburg.de>
2024-01-31 13:53:20 +0100

    switcing branches

--- src/raytrace/draw_cylinder.c
+++ src/raytrace/draw_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: Kekuhne <kekuehne@student.42wolfsburg.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 19:20:11 by yiwong            #+#    #+#             */
-/*   Updated: 2024/01/25 16:19:16 by Kekuhne          ###   ########.fr       */
+/*   Updated: 2024/01/31 13:52:18 by Kekuhne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,22 @@ t_intersect	ray_intersects_cylinder(t_xyz *viewpoint, t_xyz ray, t_cylinder *cl)
 	t_xyz		local_ray;
 	t_matrix	rotation;
 	t_intersect	intersection;
+	double		angle;
+	double		inv_angle;
 	double		discr_vars[3];
 	double		discriminant;
 	
 	intersection = new_intersect();
-	rotation = create_rotation_matrix(v_normalize(v_cross(*cl->axis, (t_xyz){0,1,0})), angle_between(v_normalize(*cl->axis),(t_xyz){0,1,0}));
+	angle = angle_between(v_normalize(*cl->axis),(t_xyz){0,1,0});
+	inv_angle = angle * -1;
+	//printf("angle = %f inv_angle = %f\n", angle, inv_angle);
+	rotation = create_rotation_matrix(v_normalize(v_cross(*cl->axis, (t_xyz){0,1,0})), angle);
 	local_cl_center = v_matrix_mul(rotation, *cl->centre);
 	local_cl_axis = v_matrix_mul(rotation, v_normalize(*cl->axis));
+	/* printf("local_axis = %f %f %f\n", local_cl_axis.x, local_cl_axis.y, local_cl_axis.z); */
 	local_viewpoint = v_matrix_mul(rotation, *viewpoint);
-	local_ray = v_matrix_mul(rotation, ray);
+	/* printf("local_viewpoint = %f %f %f\n", local_viewpoint.x, local_viewpoint.y, local_viewpoint.z); */
+	local_ray = v_matrix_mul(rotation,ray);
 	discr_vars[0] = v_dot(local_ray, local_ray) - pow(v_dot(local_ray, local_cl_axis), 2);
 	discr_vars[1] = 2 * (v_dot(local_cl_center, local_ray) - v_dot(local_ray, local_cl_axis) * v_dot(local_cl_center, local_cl_axis));
 	discr_vars[2] = v_dot(local_cl_center, local_cl_center) - pow(v_dot(local_cl_center, local_cl_axis), 2) - pow(cl->diameter / 2, 2);
@@ -55,18 +62,21 @@ t_intersect	ray_intersects_cylinder(t_xyz *viewpoint, t_xyz ray, t_cylinder *cl)
 	if (discriminant <= 0)
 		return (intersection);
 	intersection.distance = cylinder_closest_t(discriminant, discr_vars);
+	/* printf("intersection distance = %f\n", intersection.distance); */
 	intersection.point = v_add(local_viewpoint, v_scale(local_ray, intersection.distance));
-	if (intersection.point.y <= cl->centre->y - (cl->height / 2)
-		|| intersection.point.y >= cl->centre->y + (cl->height / 2))
+	//printf("intersection_point = %f %f %f\n", intersection.point.x, intersection.point.y, intersection.point.z);
+	if (intersection.point.y <= cl->centre->y + (cl->height / 2)
+		&& intersection.point.y >= cl->centre->y - (cl->height / 2))
 	{
-		rotation = create_rotation_matrix(v_normalize(v_cross((t_xyz){0,1,0},v_normalize(*cl->axis))), -angle_between((t_xyz){0,1,0}, v_normalize(*cl->axis)));
+		/* rotation = create_rotation_matrix(v_normalize(v_cross((t_xyz){0,1,0},v_normalize(*cl->axis))), -angle_between((t_xyz){0,1,0}, v_normalize(*cl->axis)));
 		intersection.point = v_matrix_mul(rotation, ray_intersects_cylinder_disk(&local_viewpoint, local_ray, cl, &intersection, local_cl_center));
 		return (intersection);
 	}
 	else
-	{
-		rotation = create_rotation_matrix(v_normalize(v_cross((t_xyz){0,1,0}, v_normalize(*cl->axis))), -angle_between((t_xyz){0,1,0}, v_normalize(*cl->axis)));
+	{ */
+		rotation = create_rotation_matrix(v_normalize(v_cross((t_xyz){0,1,0}, v_normalize(*cl->axis))), inv_angle);
 		intersection.point = v_matrix_mul(rotation, intersection.point);
+		/* printf("intersection_point = %f %f %f\n", intersection.point.x, intersection.point.y, intersection.point.z); */
 		intersection.valid = true;
 	}
 	intersection.shape = cl;
