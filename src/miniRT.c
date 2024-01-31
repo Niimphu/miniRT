c74aa22a2ecc11e8f8bf7f015c89d2fd891cdd83
Niimphu <nimphu@outlook.com>
2024-01-31 18:05:09 +0100

    commented out option to time how long program takes to render

--- src/miniRT.c
+++ src/miniRT.c
@@ -22,3 +22,24 @@ int	raytrace(t_rt *rt)
 	mlx_loop(rt->mlx_data->mlx);
 	return (0);
 }
+
+////print amount of time taken to render scene
+//#include <sys/time.h>
+//
+//int	raytrace(t_rt *rt)
+//{
+//	struct timeval	start, end;
+//	double			processing_time;
+//
+//	mlx_hook(rt->mlx_data->win, KeyPress, 1L << 0, key_pressed, rt);
+//	mlx_hook(rt->mlx_data->win, DestroyNotify, 0L, window_closed, rt);
+//	gettimeofday(&start, NULL);
+//	draw_scene(rt);
+//	gettimeofday(&end, NULL);
+//	processing_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1e6;
+//	printf("Draw time: %f seconds.\n", processing_time);
+//	mlx_expose_hook(rt->mlx_data->win, draw_scene, rt);
+//	mlx_loop(rt->mlx_data->mlx);
+//	return (0);
+//}
+//
