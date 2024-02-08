9d4f7e7a694e34bd728bc3dac0f867afb3033e48
Niimphu <nimphu@outlook.com>
2024-02-08 21:12:06 +0100

    cap chip fixed

--- src/intersect/intersect_cylinder.c
+++ src/intersect/intersect_cylinder.c
@@ -47,29 +47,32 @@ double	cyl_local_intersect(t_xyz local_viewpoint, t_xyz local_ray, t_cylinder *c
 		return (t2);
 }
 
-//t_intersect	ray_intersects_caps(t_cylinder *cylinder, t_xyz viewpoint, t_xyz ray)
+//t_intersect	ray_intersects_caps(t_cylinder *cylinder, t_xyz viewpoint, t_xyz local_viewpoint, t_xyz local_ray)
 //{
-//	t_intersect	intersect;
-//	t_xyz		centre[2];
-//	int			closest;
-//	double		centre_distance;
-//
-//	intersect = new_intersect();
-//	centre[TOP] = (t_xyz) {0, cylinder->height / 2.0, 0};
-//	centre[BOT] = (t_xyz) {0, cylinder->height / -2.0, 0};
-//	if (p2p_distance(centre[TOP], viewpoint) < p2p_distance(centre[BOT], viewpoint))
-//		closest = TOP;
-//	else
-//		closest = BOT;
-//	centre_distance = v_dot(v_subtract(centre[closest], viewpoint), (t_xyz){0, 1, 0}) / v_dot(ray, (t_xyz) {0, 1, 0});
-//	intersect.point = v_add(viewpoint, v_scale(ray, centre_distance));
-//	if (p2p_distance(centre[closest], intersect.point) <= cylinder->radius && centre_distance > TOLERANCE)
+//	t_intersect	intersection;
+//	t_xyz top_centre = (t_xyz) {0, cylinder->height / 2.0, 0};
+//	t_xyz bot_centre = (t_xyz) {0, cylinder->height / -2.0, 0};
+//	double dist_top =
+//			v_dot(v_subtract(top_centre, local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(local_ray, (t_xyz) {0, 1, 0});
+//	double dist_bot =
+//			v_dot(v_subtract(bot_centre, local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(local_ray, (t_xyz) {0, 1, 0});
+//	t_xyz top_point = v_add(local_viewpoint, v_scale(local_ray, dist_top));
+//	t_xyz bot_point = v_add(local_viewpoint, v_scale(local_ray, dist_bot));
+//	if (p2p_distance(top_centre, top_point) <= cylinder->radius && dist_top > TOLERANCE)
 //	{
-//		intersect.distance = p2p_distance(viewpoint, intersect.point);
-//		intersect.point = v_add(viewpoint, v_scale(ray, intersect.distance));
-//		if (intersect.distance > TOLERANCE)
-//			intersect.valid = true;
+//		intersection.distance = p2p_distance(local_viewpoint, top_point);
+//		intersection.point = v_add(viewpoint, v_scale(local_ray, intersection.distance));
+//		if (intersection.distance > TOLERANCE)
+//			intersection.valid = true;
 //	}
+//	else if (p2p_distance(bot_centre, bot_point) <= cylinder->radius && dist_bot > TOLERANCE)
+//	{
+//		intersection.distance = p2p_distance(local_viewpoint, bot_point);
+//		intersection.point = v_add(viewpoint, v_scale(local_ray, intersection.distance));
+//		if (intersection.distance > TOLERANCE)
+//			intersection.valid = true;
+//	}
+//	return (intersection);
 //}
 
 t_intersect	ray_intersects_cylinder(t_xyz *viewpoint, t_xyz ray, t_cylinder *cl)
@@ -98,26 +101,44 @@ t_intersect	ray_intersects_cylinder(t_xyz *viewpoint, t_xyz ray, t_cylinder *cl)
 	}
 	else
 	{
-//		intersection = ray_intersects_caps(cl, local_viewpoint, local_ray);
-//		intersection.point = v_add(*viewpoint, v_scale(ray, intersection.distance));
-		t_xyz top_centre = (t_xyz) {0, cl->height / 2.0, 0};
-		t_xyz bot_centre = (t_xyz) {0, cl->height / -2.0, 0};
-		double dist_top =
-				v_dot(v_subtract(top_centre, local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(local_ray, (t_xyz) {0, 1, 0});
-		double dist_bot =
-				v_dot(v_subtract(bot_centre, local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(local_ray, (t_xyz) {0, 1, 0});
-		t_xyz top_point = v_add(local_viewpoint, v_scale(local_ray, dist_top));
-		t_xyz bot_point = v_add(local_viewpoint, v_scale(local_ray, dist_bot));
-		if (p2p_distance(top_centre, top_point) <= cl->radius && dist_top > TOLERANCE)
+//		intersection = ray_intersects_caps(cl, *viewpoint, local_viewpoint, local_ray);
+//		t_xyz top_centre = (t_xyz) {0, cl->height / 2.0, 0};
+//		t_xyz bot_centre = (t_xyz) {0, cl->height / -2.0, 0};
+//		double dist_top =
+//				v_dot(v_subtract(top_centre, local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(local_ray, (t_xyz) {0, 1, 0});
+//		double dist_bot =
+//				v_dot(v_subtract(bot_centre, local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(local_ray, (t_xyz) {0, 1, 0});
+//		t_xyz top_point = v_add(local_viewpoint, v_scale(local_ray, dist_top));
+//		t_xyz bot_point = v_add(local_viewpoint, v_scale(local_ray, dist_bot));
+//		if (p2p_distance(top_centre, top_point) + TOLERANCE < cl->radius && dist_top > TOLERANCE)
+//		{
+//			intersection.distance = p2p_distance(local_viewpoint, top_point);
+//			intersection.point = v_add(*viewpoint, v_scale(ray, intersection.distance));
+//			if (intersection.distance > TOLERANCE)
+//				intersection.valid = true;
+//		}
+//		if (p2p_distance(bot_centre, bot_point) + TOLERANCE < cl->radius && dist_bot > TOLERANCE)
+//		{
+//			intersection.distance = p2p_distance(local_viewpoint, bot_point);
+//			intersection.point = v_add(*viewpoint, v_scale(ray, intersection.distance));
+//			if (intersection.distance > TOLERANCE)
+//				intersection.valid = true;
+//		}
+
+		int			closest;
+		t_xyz		centre[2];
+		centre[TOP] = (t_xyz) {0, cl->height / 2.0, 0};
+		centre[BOT] = (t_xyz) {0, cl->height / -2.0, 0};
+		if (p2p_distance(local_viewpoint, centre[TOP]) < p2p_distance(local_viewpoint, centre[BOT]))
+			closest = TOP;
+		else
+			closest = BOT;
+
+		double		dist = v_dot(v_subtract(centre[closest], local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(local_ray, (t_xyz) {0, 1, 0});
+		t_xyz		point = v_add(local_viewpoint, v_scale(local_ray, dist));
+		if (p2p_distance(centre[closest], point) + TOLERANCE < cl->radius && dist > TOLERANCE)
 		{
-			intersection.distance = p2p_distance(local_viewpoint, top_point);
-			intersection.point = v_add(*viewpoint, v_scale(ray, intersection.distance));
-			if (intersection.distance > TOLERANCE)
-				intersection.valid = true;
-		}
-		if (p2p_distance(bot_centre, bot_point) <= cl->radius && dist_bot > TOLERANCE)
-		{
-			intersection.distance = p2p_distance(local_viewpoint, bot_point);
+			intersection.distance = p2p_distance(local_viewpoint, point);
 			intersection.point = v_add(*viewpoint, v_scale(ray, intersection.distance));
 			if (intersection.distance > TOLERANCE)
 				intersection.valid = true;
