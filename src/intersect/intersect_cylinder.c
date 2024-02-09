f69d076eb58d31cf12f654a07f4e460e12ad6c3a
Niimphu <nimphu@outlook.com>
2024-02-09 17:15:24 +0100

    cylinders are DONE

--- src/intersect/intersect_cylinder.c
+++ src/intersect/intersect_cylinder.c
@@ -97,28 +97,6 @@ t_intersect	ray_intersects_cylinder(t_xyz *viewpoint, t_xyz ray, t_cylinder *cl)
 	else
 	{
 //		intersection = ray_intersects_caps(cl, *viewpoint, local_viewpoint, local_ray);
-//		t_xyz top_centre = (t_xyz) {0, cl->height / 2.0, 0};
-//		t_xyz bot_centre = (t_xyz) {0, cl->height / -2.0, 0};
-//		double dist_top =
-//				v_dot(v_subtract(top_centre, local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(local_ray, (t_xyz) {0, 1, 0});
-//		double dist_bot =
-//				v_dot(v_subtract(bot_centre, local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(local_ray, (t_xyz) {0, 1, 0});
-//		t_xyz top_point = v_add(local_viewpoint, v_scale(local_ray, dist_top));
-//		t_xyz bot_point = v_add(local_viewpoint, v_scale(local_ray, dist_bot));
-//		if (p2p_distance(top_centre, top_point) + TOLERANCE < cl->radius && dist_top > TOLERANCE)
-//		{
-//			intersection.distance = p2p_distance(local_viewpoint, top_point);
-//			intersection.point = v_add(*viewpoint, v_scale(ray, intersection.distance));
-//			if (intersection.distance > TOLERANCE)
-//				intersection.valid = true;
-//		}
-//		if (p2p_distance(bot_centre, bot_point) + TOLERANCE < cl->radius && dist_bot > TOLERANCE)
-//		{
-//			intersection.distance = p2p_distance(local_viewpoint, bot_point);
-//			intersection.point = v_add(*viewpoint, v_scale(ray, intersection.distance));
-//			if (intersection.distance > TOLERANCE)
-//				intersection.valid = true;
-//		}
 
 		int			closest;
 		t_xyz		centre[2];
@@ -129,15 +107,36 @@ t_intersect	ray_intersects_cylinder(t_xyz *viewpoint, t_xyz ray, t_cylinder *cl)
 		else
 			closest = BOT;
 
-		double		dist = v_dot(v_subtract(centre[closest], t.local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(t.local_ray, (t_xyz) {0, 1, 0});
-		t_xyz		point = v_add(t.local_viewpoint, v_scale(t.local_ray, dist));
-		if (p2p_distance(centre[closest], point) + TOLERANCE < cl->radius && dist > TOLERANCE)
+		t_intersect	intersect[2];
+		intersect[TOP] = new_intersect();
+		intersect[BOT] = new_intersect();
+
+		double		dist[2];
+		dist[TOP] = v_dot(v_subtract(centre[TOP], t.local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(t.local_ray, (t_xyz){0, 1, 0});
+		dist[BOT] = v_dot(v_subtract(centre[BOT], t.local_viewpoint), (t_xyz){0, 1, 0}) / v_dot(t.local_ray, (t_xyz){0, 1, 0});
+		intersect[TOP].point = v_add(t.local_viewpoint, v_scale(t.local_ray, dist[TOP]));
+		intersect[BOT].point = v_add(t.local_viewpoint, v_scale(t.local_ray, dist[BOT]));
+		if (p2p_distance(centre[TOP], intersect[TOP].point) + TOLERANCE < cl->radius && dist[TOP] > TOLERANCE)
 		{
-			intersection.distance = p2p_distance(t.local_viewpoint, point);
-			intersection.point = v_add(*viewpoint, v_scale(ray, intersection.distance));
-			if (intersection.distance > TOLERANCE)
-				intersection.valid = true;
+			intersect[TOP].distance = p2p_distance(t.local_viewpoint, intersect[TOP].point);
+			intersect[TOP].point = v_add(*viewpoint, v_scale(ray, intersect[TOP].distance));
+			if (intersect[TOP].distance > TOLERANCE)
+				intersect[TOP].valid = true;
 		}
+		if (p2p_distance(centre[BOT], intersect[BOT].point) + TOLERANCE < cl->radius && dist[BOT] > TOLERANCE)
+		{
+			intersect[BOT].distance = p2p_distance(t.local_viewpoint, intersect[BOT].point);
+			intersect[BOT].point = v_add(*viewpoint, v_scale(ray, intersect[BOT].distance));
+			if (intersect[BOT].distance > TOLERANCE)
+				intersect[BOT].valid = true;
+		}
+
+		if (intersect[TOP].valid && intersect[BOT].valid)
+			intersection = intersect[closest];
+		else if (intersect[TOP].valid)
+			intersection = intersect[TOP];
+		else
+			intersection = intersect[BOT];
 	}
 	intersection.shape = cl;
 	intersection.type = CYLINDER;
