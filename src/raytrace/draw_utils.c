c235857649a0d2f2c86cca5619509213a9092e09
Niimphu <nimphu@outlook.com>
2024-02-20 13:46:23 +0100

    vertical camera now functions correctly

--- src/raytrace/draw_utils.c
+++ src/raytrace/draw_utils.c
@@ -15,7 +15,11 @@
 
 void	calculate_camera_right_up(t_camera *camera)
 {
-	camera->right = v_normalize(v_cross(*camera->forward, (t_xyz){0, 1, 0}));
+	if (camera->forward->x == 0 && camera->forward->y == 0)
+		camera->right = (t_xyz){1, 0, 0};
+	else
+		camera->right = v_normalize(v_cross(*camera->forward,
+					(t_xyz){0, 1, 0}));
 	camera->up = v_normalize(v_cross(camera->right, *camera->forward));
 	camera->right = v_invert(camera->right);
 }
